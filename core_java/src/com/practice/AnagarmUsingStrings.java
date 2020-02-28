0ba1d91e10ec15fdc61c2bb4a862c766ea5693e2
Mayank Singh <mayanksi191op@gmail.com>
2020-02-28 12:03:00 +0530

    pure string use

--- core_java/src/com/practice/AnagarmUsingStrings.java
+++ core_java/src/com/practice/AnagarmUsingStrings.java
@@ -3,8 +3,28 @@ package com.practice;
 import java.util.Scanner;
 
 public class AnagarmUsingStrings {
-	public void AnagramString() {
-		Scanner sc=new Scanner(System.in)
+	public boolean AnagramString() {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		int count=0;
+		
+		if(a.length()==b.length())
+			return false;
+		
+		for(int i=0; i<a.length()-1; i++) {
+			i--;
+			for(int j=0; j<b.length(); j++) {
+				if((a.charAt(i)+"").equalsIgnoreCase(b.charAt(j)+"")) {
+					count++;
+				}
+				a=a.replaceFirst(a.charAt(i)+"", "");
+				b=b.replaceFirst(b.charAt(j)+"", "");
+			}
+			
+		}
+		if(count==b.length()) {
+			return true;
+		} else return false;
 	}
-	
 }
